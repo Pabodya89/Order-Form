c42f7e11811197f73bc8897a89aac4563e05b4f4
Pabodya89 <2022cs209@stu.ucsc.cmb.ac.lk>
2025-06-03 18:13:52 +0530

    "add the RequestOrderDetails to getters and setters"

--- src/main/java/com/example/demo/dto/request/RequestOrderDetailSaveDTO.java
+++ src/main/java/com/example/demo/dto/request/RequestOrderDetailSaveDTO.java
@@ -17,4 +17,44 @@ public class RequestOrderDetailSaveDTO {
     private Double amount;
     private int orders;
     private int items;
+
+    public int getItems() {
+        return items;
+    }
+
+    public void setItems(int items) {
+        this.items = items;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public double getQty() {
+        return qty;
+    }
+
+    public void setQty(double qty) {
+        this.qty = qty;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public int getOrders() {
+        return orders;
+    }
+
+    public void setOrders(int orders) {
+        this.orders = orders;
+    }
 }
